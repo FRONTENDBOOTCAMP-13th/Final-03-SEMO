1235bcb446af791159d4a3b8a1af84da7c8a4a7c
huniversal <hunivers0523@naver.com>
2025-07-13 22:29:24 +0900

    :tada: init: buyMarket 기본 페이지 뼈대 구성

--- src/app/school/buyMarket/page.tsx
+++ src/app/school/buyMarket/page.tsx
@@ -1,8 +1,8 @@
 // 상품 전체 리스트 UI 및 탭 전환 관리
 export default function buyMarket() {
   return (
-    <div className='bg-amber-400'> 
-      <h1>buyMarket 살래요 page</h1>
+    <div className='min-w-[20rem] max-w-md mx-auto px-4 bg-uni-white min-h-screen'>
+      <h1>사고 싶어요</h1>
     </div>
   );
 }
