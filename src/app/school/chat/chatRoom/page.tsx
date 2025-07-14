9f9085c2d3c636e31041125c7467d0d74f562bd0
hansol65 <whgksthf65@gmail.com>
2025-07-14 15:25:31 +0900

    ✨ feat: 개인 채팅방 page 조합완성

--- src/app/school/chat/chatRoom/page.tsx
+++ src/app/school/chat/chatRoom/page.tsx
@@ -1,11 +1,16 @@
 import ChatBubble from "../components/chatBubble/page";
+import InputChat from "../components/inputChat/page";
 import ProductInfo from "../components/productInfo/page";
+import TradeInfoBox from "../components/tradeInfoBox/page";
 
 const ChatRoom = () => {
   return (
     <>
       <ProductInfo />
       <ChatBubble />
+      <ChatBubble />
+      <TradeInfoBox />
+      <InputChat />
     </>
   );
 };
