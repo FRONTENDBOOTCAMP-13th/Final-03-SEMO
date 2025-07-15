4e038f03d11dfb3a944429ff3b7a1ec6c78ba4a7
huniversal <hunivers0523@naver.com>
2025-07-15 23:12:52 +0900

    :sparkles: feat: CommentNew 분리

--- src/app/school/market/postId/CommentNew.tsx
+++ src/app/school/market/postId/CommentNew.tsx
@@ -3,7 +3,6 @@ export default function CommentNew() {
     <div className="min-w-[320px] max-w-[480px]">
       {/* 댓글 */}
       <div>
-        <h3 className="font-bold text-22 mt-5 mb-2">댓글 (2)</h3>
         <input
           placeholder="댓글을 입력하세요"
           className="w-full bg-uni-gray-200 rounded-md p-3 mb-8 text-16"
