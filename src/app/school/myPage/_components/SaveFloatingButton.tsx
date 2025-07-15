c41b4e1c2d290f7eb08ea507493e443634cbcc6b
parksiwoon <parksw003@gmail.com>
2025-07-15 14:26:11 +0900

    :memo: docs: SaveFloatingButton 컴포넌트 jsDoc 주석 추가 - 컴포넌트 설명 및 props 정보 작성 - 사용 예시 코드 추가

--- src/app/school/myPage/_components/SaveFloatingButton.tsx
+++ src/app/school/myPage/_components/SaveFloatingButton.tsx
@@ -1,3 +1,18 @@
+/**
+ * SaveFloatingButton 컴포넌트
+ *
+ * 화면 하단에 고정되어 노출되는 저장/등록 버튼 컴포넌트입니다.
+ *
+ * @example
+ * <SaveFloatingButton onClick={handleSave}>
+ *   저장하기
+ * </SaveFloatingButton>
+ *
+ * @param children - 버튼 안에 들어갈 텍스트 혹은 컴포넌트
+ * @param onClick - 버튼 클릭 시 호출될 이벤트 핸들러
+ * @param className - 추가로 전달할 Tailwind CSS 클래스명
+ */
+
 "use client";
 
 import { ReactNode } from "react";
