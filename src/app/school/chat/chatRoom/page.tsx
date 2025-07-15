88998baa3d2da3fc09e90354bbf91b5d773d6e7f
hansol65 <whgksthf65@gmail.com>
2025-07-15 16:31:00 +0900

    ♻️ refactor: chatTest에서 chatRoom에서 채팅기능 리팩토링

--- src/app/school/chat/chatRoom/page.tsx
+++ src/app/school/chat/chatRoom/page.tsx
@@ -1,16 +1,22 @@
-import ChatBubble from "../components/chatBubble/page";
+"use client";
+
+import { useChatSocket } from "./useChatSoket";
+import ChatBubbleList from "../components/chatBubbleList/page";
 import InputChat from "../components/inputChat/page";
 import ProductInfo from "../components/productInfo/page";
-import TradeInfoBox from "../components/tradeInfoBox/page";
 
 const ChatRoom = () => {
+  const userId = "hansol65";
+  const nickName = "조한솔";
+  const roomName = "테스트방";
+
+  useChatSocket({ userId, nickName, roomName });
+
   return (
     <>
       <ProductInfo />
-      <ChatBubble />
-      <ChatBubble />
-      <TradeInfoBox />
-      <InputChat />
+      <ChatBubbleList myUserId={userId} />
+      <InputChat userId={userId} nickName={nickName} />
     </>
   );
 };
--- src/app/school/chat/components/inputChat/page.tsx
+++ src/app/school/chat/components/inputChat/page.tsx
@@ -1,16 +1,41 @@
-import { ImageIcon } from "lucide-react";
+"use client";
 
-const InputChat = () => {
+import { Send } from "lucide-react";
+import { useState } from "react";
+import { socket } from "../../chatRoom/useChatSoket";
+
+interface InputChatProps {
+  userId: string;
+  nickName: string;
+}
+
+const InputChat = ({ userId, nickName }: InputChatProps) => {
+  const [input, setInput] = useState("");
+
+  const handleSend = () => {
+    if (input.trim()) {
+      socket.emit("message", {
+        msg: input,
+        user_id: userId,
+        nickName: nickName,
+      });
+      setInput("");
+    }
+  };
+
   return (
     <div className="w-full min-w-[360px] max-w-[480px] px-4 py-3 ">
       <div className="flex items-center bg-uni-gray-200 rounded-[12px] h-12 w-full">
         <input
           type="text"
+          value={input || ""}
+          onChange={(e) => setInput(e.target.value)}
+          onKeyDown={(e) => e.key === "Enter" && handleSend()}
           placeholder="메시지 보내기"
           className="w-full bg-transparent outline-none ml-4 my-2 mr-2 placeholder-uni-gray-600 text-16 text-uni-black"
         />
-        <button className="text-uni-black ml-[6px] my-[14px] mr-[14px]">
-          <ImageIcon size={20} />
+        <button onClick={handleSend} className="text-uni-black ml-[6px] my-[14px] mr-[14px]">
+          <Send size={20} />
         </button>
       </div>
     </div>
