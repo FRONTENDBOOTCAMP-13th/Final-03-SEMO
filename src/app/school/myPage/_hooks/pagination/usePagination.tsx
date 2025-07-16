2793e1af7f9865652bf21d41498e5400a3fba728
parksiwoon <parksw003@gmail.com>
2025-07-16 13:28:50 +0900

    :memo: docs: usePagination 주석 추가 및 pagination hook README 추가 - usePagination.tsx 파일에 페이지네이션 로직에 대한 주석을 추가 - pagination hook에 대한 사용법 문서화

--- src/app/school/myPage/_hooks/pagination/usePagination.tsx
+++ src/app/school/myPage/_hooks/pagination/usePagination.tsx
@@ -15,11 +15,13 @@ export function usePagination<T>({ data, itemsPerPage, initialPage = 1 }: UsePag
 
   const totalPages = Math.ceil(data.length / itemsPerPage);
 
+  //현재 페이지에 보여줄 데이터 배열 잘라서 반환
   const paginatedData = useMemo(() => {
     const startIndex = (currentPage - 1) * itemsPerPage;
     return data.slice(startIndex, startIndex + itemsPerPage);
   }, [data, currentPage, itemsPerPage]);
 
+  //페이지 클릭 시 페이지 이동 실행
   const handlePageChange = (selectedItem: PageChangeEvent) => {
     setCurrentPage(selectedItem.selected + 1);
   };
