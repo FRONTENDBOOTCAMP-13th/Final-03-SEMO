ea454d4351e6d2f6e3dbb38885ca4bc849d164ab
parksiwoon <parksw003@gmail.com>
2025-07-16 13:08:24 +0900

    :lipstick: style: Pagination 스타일 적용 - 페이지네이션 컨테이너 flex 배치 - 페이지, 이전/다음 버튼, 활성화된 페이지 등 스타일 클래스 추가

--- src/app/school/myPage/_components/Pagination.tsx
+++ src/app/school/myPage/_components/Pagination.tsx
@@ -3,5 +3,23 @@
 import ReactPaginate from "react-paginate";
 
 export default function Pagination({ pageCount, onPageChange, forcePage = 0 }) {
-  return <ReactPaginate pageCount={pageCount} onPageChange={onPageChange} forcePage={forcePage} />;
+  return (
+    <ReactPaginate
+      pageCount={pageCount}
+      pageRangeDisplayed={3}
+      marginPagesDisplayed={1}
+      onPageChange={onPageChange}
+      // 페이지네이션 스타일 부분
+      previousLabel="‹"
+      nextLabel="›"
+      breakLabel="..."
+      containerClassName="flex space-x-2 justify-center mt-4"
+      activeClassName="bg-uni-blue-400 text-white rounded px-3 py-1"
+      pageClassName="px-3 py-1 text-gray-700 hover:bg-gray-200 rounded"
+      previousClassName="px-2 py-1 text-gray-700 hover:bg-gray-200"
+      nextClassName="px-2 py-1 text-gray-700 hover:bg-gray-200"
+      disabledClassName="text-gray-300"
+      forcePage={forcePage}
+    />
+  );
 }
