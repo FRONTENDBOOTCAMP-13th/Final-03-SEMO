ecd5b17ee4f7ca1158dfd14edc853114829ba70b
parksiwoon <parksw003@gmail.com>
2025-07-16 14:07:51 +0900

    :sparkles: feat: useResponsivePagination 기본 상태 및 pagination 로직 추가 - currentPage 상태 추가 - totalPages 계산 - paginatedData 계산 로직 구현

--- src/app/school/myPage/_hooks/pagination/useResponsivePagination.tsx
+++ src/app/school/myPage/_hooks/pagination/useResponsivePagination.tsx
@@ -12,3 +12,34 @@ interface UseResponsivePaginationProps<T> {
 interface PageChangeEvent {
   selected: number;
 }
+
+export function useResponsivePagination<T>({
+  data,
+  estimatedItemHeight = 100,
+  minItemsPerPage = 3,
+  maxItemsPerPage = 10,
+  initialPage = 1,
+  reservedHeight = 200,
+}: UseResponsivePaginationProps<T>) {
+  const [currentPage, setCurrentPage] = useState(initialPage);
+
+  const totalPages = Math.ceil(data.length / minItemsPerPage);
+
+  const paginatedData = useMemo(() => {
+    const startIndex = (currentPage - 1) * minItemsPerPage;
+    return data.slice(startIndex, startIndex + minItemsPerPage);
+  }, [data, currentPage, minItemsPerPage]);
+
+  const handlePageChange = (selectedItem: PageChangeEvent) => {
+    setCurrentPage(selectedItem.selected + 1);
+  };
+
+  return {
+    currentPage,
+    totalPages,
+    paginatedData,
+    handlePageChange,
+    setCurrentPage,
+    hasData: data.length > 0,
+  };
+}
