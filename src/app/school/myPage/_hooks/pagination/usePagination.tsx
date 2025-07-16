7fe0220ffe11cc80c5847475d6d6f784851868c3
parksiwoon <parksw003@gmail.com>
2025-07-16 13:10:29 +0900

    :sparkles: feat: usePagination 훅에 데이터 슬라이싱 로직 추가 - 현재 페이지 기준 데이터 잘라내는 paginatedData 반환 - 총 페이지 수 계산하여 totalPages 제공

--- src/app/school/myPage/_hooks/pagination/usePagination.tsx
+++ src/app/school/myPage/_hooks/pagination/usePagination.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 
 interface UsePaginationProps<T> {
   data: T[];
@@ -9,8 +9,17 @@ interface UsePaginationProps<T> {
 export function usePagination<T>({ data, itemsPerPage, initialPage = 1 }: UsePaginationProps<T>) {
   const [currentPage, setCurrentPage] = useState(initialPage);
 
+  const totalPages = Math.ceil(data.length / itemsPerPage);
+
+  const paginatedData = useMemo(() => {
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    return data.slice(startIndex, startIndex + itemsPerPage);
+  }, [data, currentPage, itemsPerPage]);
+
   return {
     currentPage,
+    totalPages,
+    paginatedData,
     setCurrentPage,
   };
 }
