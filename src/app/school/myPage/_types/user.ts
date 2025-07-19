3a82f19f38a6d92c896d64529d2f12829d51fa3a
parksiwoon <parksw003@gmail.com>
2025-07-20 03:36:03 +0900

    :sparkles: feat: API 응답 타입 인터페이스 정의 - LoginResponse 인터페이스 추가 - 토큰 정보 포함한 로그인 응답 구조

--- src/app/school/myPage/_types/user.ts
+++ src/app/school/myPage/_types/user.ts
@@ -2,6 +2,21 @@
  * 마이페이지 관련 타입 정의
  */
 
+// 로그인 API 응답의 구조
+export interface LoginResponse {
+  ok: number;
+  item: {
+    _id: number;
+    email: string;
+    name: string;
+    type: string;
+    token: {
+      accessToken: string;
+      refreshToken: string;
+    };
+  };
+}
+
 // 사용자 정보의 기본 구조
 export interface User {
   _id: number;
