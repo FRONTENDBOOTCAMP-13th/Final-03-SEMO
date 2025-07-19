e47c6806c54071998be9b16575ac5308bb123f7a
parksiwoon <parksw003@gmail.com>
2025-07-20 04:45:59 +0900

    💄 style: SVG 색상 커스텀 스타일에 맞춰 재정의

--- src/app/school/myPage/(settings)/account/page.tsx
+++ src/app/school/myPage/(settings)/account/page.tsx
@@ -85,7 +85,7 @@ export default function MyPageAccount() {
       );
     }
     return (
-      <svg className="w-14 h-14 text-gray-400" fill="currentColor" viewBox="0 0 24 24">
+      <svg className="w-14 h-14 text-uni-gray-400" fill="currentColor" viewBox="0 0 24 24">
         <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" />
       </svg>
     );
