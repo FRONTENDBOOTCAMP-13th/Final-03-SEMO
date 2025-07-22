719f8ff9a9f9197acb12544f3b525f29cfa7fbb6
parksiwoon <parksw003@gmail.com>
2025-07-22 14:15:11 +0900

    ✨ feat: 서비스 모듈을 위한 index.ts 파일 추가

---
+++ src/app/school/myPage/_services/index.ts
@@ -0,0 +1,6 @@
+
+export { default as AuthService } from './authService';
+export { default as apiClient } from './apiClient';
+export { default as UserService } from './account/userService';
+export { default as ImageService } from './imageService';
+
