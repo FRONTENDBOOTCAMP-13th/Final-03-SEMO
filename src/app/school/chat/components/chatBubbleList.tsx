6ce54d94366be9bf4166044598cf79329f571281
hansol65 <whgksthf65@gmail.com>
2025-07-23 23:49:28 +0900

    🐛 fix: 자기 메시지인지 인지 못하는 버그해결

--- src/app/school/chat/components/chatBubbleList.tsx
+++ src/app/school/chat/components/chatBubbleList.tsx
@@ -1,21 +1,23 @@
 "use client";
 
 import { useEffect, useRef } from "react";
-import { useSearchParams } from "next/navigation";
+// import { useSearchParams } from "next/navigation";
 import { useChatStore } from "../../../api/chat/useChatStore";
 import ChatBubble from "./chatBubble";
+import { useUserStore } from "@/store/userStore";
 
-interface ChatBubbleListProps {
-  myUserId: string | number;
-  myNickName?: string;
-}
+// interface ChatBubbleListProps {
+//   myUserId: string | number;
+//   myNickName?: string;
+// }
 
-const ChatBubbleList = ({ myUserId, myNickName }: ChatBubbleListProps) => {
+const ChatBubbleList = (/*{ myUserId, myNickName }: ChatBubbleListProps*/) => {
   const messages = useChatStore((state) => state.messages);
   const scrollRef = useRef<HTMLDivElement>(null);
-  const searchParams = useSearchParams();
+  // const searchParams = useSearchParams();
+  const user = useUserStore((state) => state.user);
 
-  const currentNickName = myNickName || searchParams.get("nickName") || undefined;
+  // const currentNickName = myNickName || searchParams.get("nickName") || undefined;
 
   // 새 메시지가 올 때마다 스크롤을 맨 아래로
   useEffect(() => {
@@ -23,7 +25,7 @@ const ChatBubbleList = ({ myUserId, myNickName }: ChatBubbleListProps) => {
   }, [messages]);
 
   const isMyMessage = (msg: any) => {
-    return currentNickName ? msg.nickName === currentNickName : String(msg.user_id).trim() === String(myUserId).trim();
+    return String(msg.user_id) === String(user._id) || msg.nickName === user.name;
   };
 
   return (
