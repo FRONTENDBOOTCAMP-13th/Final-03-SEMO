afd3e5c98c756c2cb5c1a13261bb3b9bf155ba33
JJukkuri <wlrn3749@naver.com>
2025-07-26 01:55:56 +0900

    🧪 test: 카카오 로그인 테스트 종료로 페이지 /school로 이동하던 주석 제거

--- src/app/(auth)/login/kakao/page.tsx
+++ src/app/(auth)/login/kakao/page.tsx
@@ -80,7 +80,7 @@ export default function KakaoLoginCallback() {
           }
 
           setUser(loginData.item);
-          // router.replace("/school");
+          router.replace("/school");
           return;
         }
 
