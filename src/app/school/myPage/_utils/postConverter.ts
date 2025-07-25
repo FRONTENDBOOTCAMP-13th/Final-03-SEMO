28b5c08761f8d601eeadd5a42d6856e35376bbb5
parksiwoon <parksw003@gmail.com>
2025-07-25 14:56:08 +0900

    ✨ feat: MyPageItem 및 Review 인터페이스 추가

--- src/app/school/myPage/_utils/postConverter.ts
+++ src/app/school/myPage/_utils/postConverter.ts
@@ -1,2 +1,20 @@
 import { BookmarkItem, OrderItem, ProductItem } from "../_types/apiResponse";
 import { getUserById } from "../_services/user"; // getUserById import
+
+export interface MyPageItem {
+  id: number;
+  title: string;
+  image: string;
+  price: string;
+  status: "판매중" | "판매완료";
+  category: "팔래요" | "살래요" | "모여요";
+}
+
+export interface Review {
+  id: number;
+  title: string;
+  author: string;
+  image: string;
+  location?: string; // 위치 정보는 선택적
+  date: string;
+}
