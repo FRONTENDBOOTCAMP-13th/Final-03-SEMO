62018d5177addb26914e7481a43a32720e8f82a9
hansol65 <whgksthf65@gmail.com>
2025-08-05 00:48:50 +0900

    🐛 fix: 상품 이미지 오류 해결

--- src/app/school/chat/components/productInfo.tsx
+++ src/app/school/chat/components/productInfo.tsx
@@ -42,13 +42,11 @@ const ProductInfo = ({ productId }: ProductInfoProps) => {
     if (productId) fetchProduct();
   }, [productId]);
 
-  const fullImageUrl = product?.image
-    ? `${process.env.NEXT_PUBLIC_API_URL}/${product.image}`
-    : "/assets/defaultImg.png";
+  const fullImageUrl = product?.image || "/assets/defaultImg.png";
 
   return (
     <div className="flex items-center gap-4 px-4 py-2">
-      <Image src={fullImageUrl} alt="판매자 이미지" width={56} height={56} className="rounded-md object-cover" />
+      <Image src={fullImageUrl} alt="거래 물품" width={56} height={56} className="rounded-md object-cover" />
       <div className="flex flex-col">
         <p className="text-16 font-semibold text-uni-black">판매자: {product?.user?.name || "정보 없음"}</p>
         <p className="text-14 text-uni-gray-600">거래 물품: {product?.title || "정보 없음"}</p>
