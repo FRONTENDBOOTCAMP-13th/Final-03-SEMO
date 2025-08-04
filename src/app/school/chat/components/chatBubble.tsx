e41bbd1e8315d366e00959b5f96fd2d9a2639f78
hansol65 <whgksthf65@gmail.com>
2025-08-05 00:48:26 +0900

    🐛 fix: 채팅말풍선 간격 조정

--- src/app/school/chat/components/chatBubble.tsx
+++ src/app/school/chat/components/chatBubble.tsx
@@ -31,7 +31,7 @@ const ChatBubble = ({ msg }: ChatBubbleProps) => {
 
   return msg.isMine ? (
     // 내가 보낸 메시지
-    <div className="flex justify-end items-end gap-2 min-h-[104px] p-4">
+    <div className="flex justify-end items-end gap-2  p-4">
       <div className="max-w-[70%] text-right">
         {isWhisper && msg.toNickName && <div className="text-12 text-uni-gray-500 mb-1">귓속말 → {msg.toNickName}</div>}
         <div className="px-4 py-3 rounded-xl text-16 break-words whitespace-pre-wrap bg-uni-blue-400 text-uni-white">
@@ -41,7 +41,7 @@ const ChatBubble = ({ msg }: ChatBubbleProps) => {
     </div>
   ) : (
     // 상대방이 보낸 메시지
-    <div className="flex justify-start items-end gap-2 min-h-[104px] p-4">
+    <div className="flex justify-start items-end gap-2 p-4">
       <div>
         <div className="w-[40px] h-[40px]">
           <Image
